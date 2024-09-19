c75a022d31fadc0bb0af6da5c26119c0d9da9a78
Sérgio Matheus <sergiomatheuss09@gmail.com>
2024-09-19 13:18:16 -0300

    Initial Commit

---
+++ Aula03/Aula03App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula03App.swift
+//  Aula03
+//
+//  Created by Turma01-25 on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Aula03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aula03/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aula03
+//
+//  Created by Turma01-25 on 19/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
