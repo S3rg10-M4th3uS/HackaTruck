f2eb9c90ac9c58ff15f9f727a5aca50bfe5db8ac
Turma01-25 <turma01-25@HackaTruck25.local>
2024-09-20 16:44:49 -0300

    To be finished: Aula 04

---
+++ aula_04/aula04/aula04/aula04App.swift
@@ -0,0 +1,17 @@
+//
+//  aula04App.swift
+//  aula04
+//
+//  Created by Turma01-25 on 20/09/24.
+//
+
+import SwiftUI
+
+@main
+struct aula04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
